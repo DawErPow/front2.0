d2c8b8fec77615918821db63d4d2160f937cb576
PEPEXf <egorkakamenskiy@gmail.com>
2024-05-28 22:20:31 +0500

    27
    27

--- app/TDS/page.js
+++ app/TDS/page.js
@@ -1,8 +1,8 @@
 'use client';
-import { useGetDataByCategory } from "../api/api-hooks"; 
+import { endpoints } from "@/app/api/config";
 import { CardsListSection } from "../components/CardsListSection/CardsListSection";
-import { endpoints } from "../api/config"; 
-import { Preloader } from "../Preloader/Preloader"; 
+import { useGetDataByCategory } from "@/app/api/api-hooks";
+import { Preloader } from "@/app/components/Preloader/Preloader";
 
 export default function TDS() {
     const newGames = useGetDataByCategory(endpoints.games, "TDS");
